2c48fb2d9f7880ef6ea14a7a040b5c1d7356bd94
ismail-hasan <fnismailhasan@gmail.com>
2025-04-03 09:51:42 +0600

    add

---
+++ src/Pages/Welcome.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Welcome = () => {
+    return (
+
+        <div className="flex items-center justify-center h-screen bg-gray-900 text-white">
+            <h1 className="text-4xl font-bold">Welcome to BIKERP</h1>
+
+        </div>
+    );
+};
+
+export default Welcome;
